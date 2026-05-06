46dfc0ea466dc824f140cab87c3954b13f9184dd
mya63 <muhammedyunusamini4@gmail.com>
2026-05-06 18:29:19 +0200

    fix cors 5501

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,9 @@ ALLOWED_HOSTS = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
+    "http://127.0.0.1:5501",
     "http://localhost:5500",
+    "http://localhost:5501",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
